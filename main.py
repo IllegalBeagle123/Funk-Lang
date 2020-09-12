39e0aaf237b5e218043ba2d371e001a74284454f
IllegalBeagle123 <pugdogbeagledog@gmail.com>
2020-09-12 07:14:08 +0000

    Implemented strings and $= (stringify)

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import re
 file = "input"
 
 with open(file + '.fl') as f:
-  input = iter([''.join(line.split()) for line in f.read().split(';')])
+  input = iter([re.sub(r'\s+(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)','',line) for line in f.read().split(';')])
 
 def run(code, *args):
-  matches = re.finditer('[a-zA-Z]+(?!(\(|[a-zA-Z]))',code)
+  matches = re.finditer(r'[a-zA-Z]+(?!\s*([\(\"a-zA-Z]))',code)
   for var in matches:
-    code = re.sub('(?<![\w])'+var.group(0)+'(?![\w])',vars[var.group(0)],code)
-  while not re.match('^(\(*-?\d+(\.\d+)?\)*[+\-*\/]?)+$',code):
-    matches = re.finditer('([a-zA-Z]+)\(((-?\d+)(,-?\d+)*)\)',code)
+    code = re.sub(r'(?<![\w])'+var.group(0)+'(?![\w])',vars[var.group(0)],code)
+  while not re.match(r'^((\(*(-?\d+|\".+\")(\.\d+)?\)*[+\-*\/]?)+)$',code):
+    matches = re.finditer(r'([a-zA-Z]+)\(((-?\d+)(,-?\d+)*)\)',code)
     for var in matches:
       if var.group(1) == 'args':
         code = code.replace(var.group(0),args[int(var.group(2))])
@@ -21,7 +21,7 @@ def run(code, *args):
 
 vars = {}
 for line in input:
-  match = re.match('([a-zA-Z]+)(\S?)=(\S+)', line)
+  match = re.match(r'([a-zA-Z]+)\s*(\S?)=(.+)', line)
   if match:
     if not match.group(2):
       vars[match.group(1)] = match.group(3)
@@ -39,5 +39,7 @@ for line in input:
     elif match.group(2) == '<':
       if not run(vars[match.group(1)]) <= run(match.group(3)):
         next(input)
+    elif match.group(2) == '$':
+      vars[match.group(1)] = f"\"{str(run(match.group(3)))}\""
     if match.group(1) == 'out':
       print(vars["out"])
